95d27b756640420f019efe2a5762c7ed5be61a68
aman-anand392000 <84987273+aman-anand392000@users.noreply.github.com>
2021-06-06 15:44:32 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1> Javascipt and React.js </h1>
+      <p>
+        This was an amazing bootcamp taken up by shaurya sinha. we covered
+        everything from scrach including Javascipt,react.js and HTML.
+      </p>
+    </div>
+  );
+}
+export default Note;
